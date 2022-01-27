2e1a84b57ec342244abcfdaf0c934d1e4c723f55
Victor <vicvilkov@yandex.ru>
2022-01-27 17:14:30 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,82 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import AddTask from './Add-task';
-import TodoList from './TodoList'
+import React from 'react';
 
-import Button from "./Button";
-import { ThemeContextConsumer } from "./themeContext";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
+import PageIndex from '../src/pages/page_index';
+import PageAbout from '../src/pages/page_about';
 
 export default function App() {
 
-  const [isLoad, setIsLoad] = useState(true);
-
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=4")
-      .then((response) => response.json())
-      .then((json) => json.map((item) => ({ id: item.id, completed: false, title: item.title, subtitle: ((Math.random() - 0.75) >= 0) ? item.title : '' })))
-      .then((json) => (setTodos(json)))
-  }, [])
-
-  const changeComplete = (id) => {
-    const copyTodos = todos;
-
-    const index = copyTodos.findIndex((item) => item.id === id)
-
-    copyTodos[index].completed = !copyTodos[index].completed;
-
-    setTodos(copyTodos);
-
-    console.log(copyTodos[index].completed)
-  }
-
-  const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
-
-  const addTask = (userInput) => {
-    if (userInput) {
-      const newItem = {
-        id: Math.random().toString(36).substr(2, 9),
-        status: 'backlog',
-        title: userInput,
-        isCompleted: false,
-      };
-
-      setTodos((prevState) => {
-        return [...prevState, newItem];
-      });
-    }
-  };
-
   return (
-    <ThemeContextConsumer>
-      {context => (
-        <div className={`layout-${context.theme}`}>
 
-          <Button />
+    <BrowserRouter>
+      <Routes>
 
-          <div className="container">
+        <Route path='/' element={<PageAbout />}>
+        </Route>
 
-            <header className='header'>
-              <h1>Todo list</h1>
-            </header>
+        <Route path='/todo' element={<PageIndex />}>
+        </Route>
 
-            <AddTask addTask={addTask} />
-
-            <button className='button__danger' onClick={() => setIsLoad(false)}>Загрузить</button>
-
-            <TodoList changeComplete={changeComplete} todos={todos} onRemove={removeTask} isLoad={isLoad} />
-
-          </div>
-
-        </div>
-      )}
-    </ThemeContextConsumer>
+      </Routes>
+    </BrowserRouter>
   );
-
 }
-
-App.contextType = ThemeContextConsumer;
---
+++ src/pages/page_about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import About from '../about';
+
+export default function PageAbout() {
+    return (
+
+        <About />
+
+    );
+}
